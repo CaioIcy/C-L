5f454bfe0e7cdf85b8d73747fc3813145198a030
ricardogtx <ricardocdzg@hotmail.com>
2013-09-09 20:04:55 -0300

    Update seguranca.php
    identação e comentários

--- cel/aplicacao/seguranca.php
+++ cel/aplicacao/seguranca.php
@@ -2,30 +2,27 @@
 
 // Escapa os metacaracteres do PHP
 
-function escapa_metacaracteres ( $string )
-{
-	$string = ereg_replace("[][{}()*+?.\\^$|]", "\\\\0", $string);
-	return $string;
+function escapa_metacaracteres($string) {
+    $string = ereg_replace("[][{}()*+?.\\^$|]", "\\\\0", $string);
+    return $string;
 }
 
-function prepara_dado( $string ) 
-{
-	//Remove espaços em branco do inicio e do fim da string
-	//$string = trim( $string );
-	
-	// Substitui o & por amp; (para que não de problemas ao gerar o XML)
-	
-	$string = ereg_replace("&", "&amp;", $string);
-	
-	// Retira tags html e php da string
-	
-	$string = strip_tags($string);
-	
-	// Verifica se a diretiva get_magic_quotes_gpc() esta ativada, se estiver a função stripslashes é utilizada na string
-	
-	$string = get_magic_quotes_gpc() ? stripslashes($string) : $string;
-	$string = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($string) : mysql_escape_string($string);
-	return $string;
-}
+//Remove espaÃ§os em branco do inicio e do fim da string
+function prepara_dado($string) {
 
+
+    // Substitui o & por amp; (para que nÃ£o de problemas ao gerar o XML)
+
+    $string = ereg_replace("&", "&amp;", $string);
+
+    // Retira tags html e php da string
+
+    $string = strip_tags($string);
+
+    // Verifica se a diretiva get_magic_quotes_gpc() esta ativada, se estiver a funÃ§Ã£o stripslashes Ã© utilizada na string
+
+    $string = get_magic_quotes_gpc() ? stripslashes($string) : $string;
+    $string = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($string) : mysql_escape_string($string);
+    return $string;
+}
 ?>
