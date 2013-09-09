27c8623fa270d698190a01d3094499ea651cae51
ricardogtx <ricardocdzg@hotmail.com>
2013-09-09 19:38:25 -0300

    Update rmv_cenario.php

--- cel/aplicacao/rmv_cenario.php
+++ cel/aplicacao/rmv_cenario.php
@@ -17,19 +17,19 @@ inserirPedidoRemoverCenario($_SESSION['id_projeto_corrente'], $id_cenario, $_SES
     opener.parent.frames['text'].location.replace('main.php?id_projeto=<?= $_SESSION['id_projeto_corrente'] ?>');
 
 <?php
-/* Cenário -  Excluir Cenário 
-Objetivo:   Permitir ao Usuário Excluir um cenário que esteja ativo
-Contexto:   Usuário deseja excluir um cenário
-Pré-Condição:   Login, cenário cadastrado no sistema
-Atores: Usuário, Sistema
-Recursos:   Dados informados
-Episódios:  O sistema fornecerá uma tela para o usuário justificar a necessidade daquela
-exclusão para que o administrador possa ler e aprovar ou não a mesma.
-Esta tela também conterá um botão para a confirmação da exclusão.
-Restrição: Depois de clicar no botão, o sistema verifica se todos os campos foram preenchidos 
-Exceçãoo:    Se todos os campos não foram preenchidos, retorna para o usuário uma mensagem
-              avisando que todos os campos devem ser preenchidos e um botão de voltar para a pagina anterior.
-?>*/
+// Cenário -  Excluir Cenário 
+//Objetivo:	Permitir ao Usuário Excluir um cenário que esteja ativo
+//Contexto:	Usuário deseja excluir um cenário
+//              Pré-Condição: Login, cenário cadastrado no sistema
+//Atores:	Usuário, Sistema
+//Recursos:	Dados informados
+//Episódios:	O sistema fornecerá uma tela para o usuário justificar a necessidade daquela
+//              exclusão para que o administrador possa ler e aprovar ou não a mesma.
+//              Esta tela também conterá um botão para a confirmação da exclusão.
+//              Restrição: Depois de clicar no botão, o sistema verifica se todos os campos foram preenchidos 
+//Exceção:	Se todos os campos não foram preenchidos, retorna para o usuário uma mensagem
+//              avisando que todos os campos devem ser preenchidos e um botão de voltar para a pagina anterior.
+?>
 
 </script>
 
