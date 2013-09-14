1b1d9995000dbf9bf089b502001a3058bc818967
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2013-09-13 22:21:34 -0300

    renaming functions and variables names of 'remove_projeto.php' class

--- cel/aplicacao/remove_projeto.php
+++ cel/aplicacao/remove_projeto.php
@@ -12,7 +12,7 @@ include("httprequest.inc");
 //Atores:	   Administrador
 //Recursos:	   Sistema, dados do projeto, base de dados
 //Epis�dios:   O Administrador clica na op��o �remover projeto� encontrada no menu superior.
-//             O sistema disponibiliza uma tela para o administrador ter certeza de que esta removendo o projeto correto.
+//             O sistema disponibiliza uma tela para o administrador ter certeza de que esta removendo o projeto correto
 //             O Administrador clica no link de remo��o.
 //             O sistema chama a p�gina que remover� o projeto do banco de dados.
 ?>
@@ -22,15 +22,15 @@ include("httprequest.inc");
     </head>
 <?php
 $id_project = $_SESSION['id_projeto_corrente'];
-$id_usuario = $_SESSION['id_usuario_corrente'];
+$id_user = $_SESSION['id_usuario_corrente'];
 
-$db_conection = bd_connect() or die("Erro ao conectar ao SGBD");
+$database_conection = bd_connect() or die("Erro ao conectar ao SGBD");
 $qv = "SELECT * FROM projeto WHERE id_projeto = '$id_project' ";
 $qvr = mysql_query($qv) or die("Erro ao enviar a query de select no projeto");
-$resultArrayProjeto = mysql_fetch_array($qvr);
-$nome_Projeto = $resultArrayProjeto[1];
-$data_Projeto = $resultArrayProjeto[2];
-$descricao_Projeto = $resultArrayProjeto[3];
+$result_array_projecto = mysql_fetch_array($qvr);
+$project_name = $result_array_projecto[1];
+$data_project = $result_array_projecto[2];
+$project_description = $result_array_projecto[3];
 ?>    
     <body>
         <h4>Remover Projeto:</h4>
@@ -44,9 +44,9 @@ $descricao_Projeto = $resultArrayProjeto[3];
                 <td width="42%"><b>Descri&ccedil;&atilde;o</b></td>
             </tr>
             <tr> 
-                <td width="29%"><?php echo $nome_Projeto; ?></td>
-                <td width="29%"><?php echo $data_Projeto; ?></td>
-                <td width="42%"><?php echo $descricao_Projeto; ?></td>
+                <td width="29%"><?php echo $project_name; ?></td>
+                <td width="29%"><?php echo $data_project; ?></td>
+                <td width="42%"><?php echo $project_description; ?></td>
             </tr>
         </table>
         <br><br>
