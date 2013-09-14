86cbbb9bc80bb80f763f843d0762dc6186215130
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-09-14 13:26:20 -0300

    updating add_project.php (language and details)

--- cel/aplicacao/add_projeto.php
+++ cel/aplicacao/add_projeto.php
@@ -4,11 +4,14 @@ session_start();
 include("funcoes_genericas.php");
 include("httprequest.inc");
 
-// Cen�rio controle de acesso
+// Access control scenario
 check_use_authentication("index.php");
 
-// Este script eh chamado quando ocorre uma solicitacao de inclusao
-// de novo projeto, ou quando um novo usuario se cadastra no sistema
+/*
+ * This script is called when there is an inclusion solicitation of a new
+ * project, or when a new user registers in the system
+ */
+
 //Cen�rio  -  Cadastrar Novo Projeto 
 //Objetivo:	   Permitir ao usu�rio cadastrar um novo projeto
 //Contexto:	   Usu�rio deseja incluir um novo projeto na base de dados
@@ -23,26 +26,28 @@ check_use_authentication("index.php");
 //              para este novo projeto.
 //Exce��o:	   Se for especificado um nome de projeto j� existente e que perten�a ou tenha a participa��o
 //                 deste usu�rio, o sistema exibe uma mensagem de erro.
-// Chamado atraves do botao de submit
+
+
+// Called through the submit button
 if (isset($submit)) {
 
     $id_includedProject = inclui_projeto($project_name, $project_description);
 
-    // Inserir na tabela participa
+    // Insert in the main table
 
     if ($id_includedProject != -1) {
-        $database_conection = database_connect() or die("Erro ao conectar ao SGBD");
+        $database_conection = database_connect() or die("Error while connecting to SGBD");
         $manager = 1;
         $id_currentUser = $_SESSION['id_currentUser'];
         $query = "INSERT INTO participa (id_user, id_project, manager) VALUES ($id_currentUser, $id_includedProject, $manager)";
-        mysql_query($query) or die("Erro ao inserir na tabela participa");
+        mysql_query($query) or die("Error while connection to the main table");
     } else {
         ?>
         <html>
-            <title>Erro</title>
+            <title>Error</title>
             <body>
-                <p style="color: red; font-weight: bold; text-align: center">Nome de projeto j� existente!</p>
-            <center><a href="JavaScript:window.history.go(-1)">Voltar</a></center>
+                <p style="color: red; font-weight: bold; text-align: center">Project name already exists!</p>
+            <center><a href="JavaScript:window.history.go(-1)">Return</a></center>
         </body>
         </html>   
         <?php
@@ -57,18 +62,18 @@ if (isset($submit)) {
     </script>
 
     <?php
-// Chamado normalmente
+// Called normally
 } else {
     ?>
 
     <html>
         <head>
-            <title>Adicionar Projeto</title>
+            <title>Add project</title>
             <script language="javascript1.3">
 
                 function checks_textArea() {
                     if (document.forms[0].nome.value == "") {
-                        alert('Preencha o campo "Nome"');
+                        alert('Fill the field "Name"');
                         document.forms[0].nome.focus();
                         return false;
                     } else {
@@ -76,7 +81,7 @@ if (isset($submit)) {
                         nOK = padrao.exec(document.forms[0].nome.value);
                         if (nOK)
                         {
-                            window.alert("O nome do projeto n�o pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
+                            window.alert("The name of the project must not contain any of the follwoing characters:   / \\ : ? \" < > |");
                             document.forms[0].nome.focus();
                             return false;
                         }
@@ -87,23 +92,23 @@ if (isset($submit)) {
             </script>
         </head>
         <body>
-            <h4>Adicionar Projeto:</h4>
+            <h4>Add project:</h4>
             <br>
             <form action="" method="post" onSubmit="return checks_textArea();">
                 <table>
                     <tr>
-                        <td>Nome:</td>
+                        <td>Name:</td>
                         <td><input maxlength="128" name="nome" size="48" type="text"></td>
                     </tr>
                     <tr>
-                        <td>Descri��o:</td>
+                        <td>Description:</td>
                         <td><textarea cols="48" name="descricao" rows="4"></textarea></td>
                     <tr>
                         <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Adicionar Projeto"></td>
                     </tr>
                 </table>
             </form>
-            <br><i><a href="showSource.php?file=add_projeto.php">Veja o c�digo fonte!</a></i>
+            <br><i><a href="showSource.php?file=add_projeto.php">See the source code!</a></i>
         </body>
     </html>
 
