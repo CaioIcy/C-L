4d8dfc9422ecf4715ff3530e96c50a7ade7c44bd
paulohtfs <paulohtfs@gmail.com>
2013-09-13 21:53:43 -0300

    Update "auxiliar_algoritmo.php" - Variable names

--- cel/aplicacao/auxiliar_algoritmo.php
+++ cel/aplicacao/auxiliar_algoritmo.php
@@ -1,18 +1,18 @@
 <?php
 
-function existe_relacao($rel, $list) {
-    foreach ($list as $key => $relacao) {
-        if (@$relacao->verbo == $rel) {
-            return $key;
+function existe_relacao($relation, $relation_list) {
+    foreach ($relation_list as $relation_key => $relacao) {
+        if (@$relacao->verbo == $relation) {
+            return $relation_key;
         }
     }
     return -1;
 }
 
-function existe_conceito($conc, $list) {
-    foreach ($list as $key => $conc1) {
-        if ($conc1->nome == $conc) {
-            return $key;
+function existe_conceito($new_concept, $concept_list) {
+    foreach ($concept_list as $concept_key => $concept) {
+        if ($concept->nome == $new_concept) {
+            return $concept_key;
         }
     }
     return -1;
