90b23d4e1b7f75c63631f7c4bc426d7620371cf8
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-09-14 13:04:05 -0300

    updating add_cenario.php language amongst few details

--- cel/aplicacao/add_cenario.php
+++ cel/aplicacao/add_cenario.php
@@ -1,20 +1,22 @@
 <?php
 session_start();
 
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// vim: set expandtab tabstop=4 shiftwidth=4:
 
-// add_cenario.php: Este script cadastra um novo cenario do projeto. Eh
-//                  passada, atraves da URL, uma variavel $id_projeto, que
-//                  indica em que projeto deve ser inserido o novo cenario.
+/*
+ * This script registers a new scenario of the project.
+ * A variable &id_project is recieved through a URL, and indicates in which
+ * project the new scenario will be inserted in.
+ */
 
 include("funcoes_genericas.php");
 include("httprequest.inc");
 include_once("bd.inc");
 
-check_use_authentication("index.php");        // Checa se o usuario foi autenticado
+check_use_authentication("index.php");        // Checks if the user was authenticated
 
-if (!isset($sucesso)) {
-    $sucesso = "n";
+if (!isset($success)) {
+    $success = "n";
 }
 
 // Conecta ao SGBD
@@ -22,15 +24,19 @@ $database_conection = database_connect() or die("Erro ao conectar ao SGBD");
 
 if (isset($submit)) {
     $is_existingScenario = checarCenarioExistente($_SESSION['id_projeto_corrente'], $scene_title);
-    ?>  <!-- ADICIONEI ISTO PARA TESTES -->
+    ?>  
+
+    <!-- find out if this interferes in the functionality of C-L -->
+    <!-- ADICIONEI ISTO PARA TESTES -->
     <!--
        RET = <?= $is_existingScenario ?> => RET = <?PHP $is_existingScenario ? print("TRUE")  : print("FALSE") ; ?><BR>
-    $sucesso        = <?= $sucesso ?><BR>
+    $sucesso        = <?= $success ?><BR>
     _GET["sucesso"] = <?= $_GET["sucesso"] ?><BR>   
     -->
+
     <?PHP
     if ($is_existingScenario == true) {
-        print("<!-- Tentando Inserir Cenario --><BR>");
+        print("<!-- Trying to insert scenario --><BR>");
 
         /* Substitui todas as ocorrencias de ">" e "<" por " " */
         $scene_title = str_replace(">", " ", str_replace("<", " ", $scene_title));
@@ -41,14 +47,14 @@ if (isset($submit)) {
         $scene_exception = str_replace(">", " ", str_replace("<", " ", $scene_exception));
         $scene_episode = str_replace(">", " ", str_replace("<", " ", $scene_episode));
         inserirPedidoAdicionarCenario($_SESSION['id_projeto_corrente'], $scene_title, $scene_goal, $scene_context, $scene_performer, $scene_resource, $scene_exception, $scene_episode, $_SESSION['id_usuario_corrente']);
-        print("<!-- Cenario Inserido Com Sucesso! --><BR>");
+        print("<!-- Scenario inserted with success! --><BR>");
     } else {
         ?>
-        <html><head><title>Projeto</title></head><body bgcolor="#FFFFFF">
-                <p style="color: red; font-weight: bold; text-align: center">Este cen�rio j� existe!</p>
+        <html><head><title>Project</title></head><body bgcolor="#FFFFFF">
+                <p style="color: red; font-weight: bold; text-align: center">This scenario already exists!</p>
                 <br>
                 <br>
-            <center><a href="JavaScript:window.history.go(-1)">Voltar</a></center>
+            <center><a href="JavaScript:window.history.go(-1)">Return</a></center>
         </body></html>
         <?php
         return;
@@ -59,26 +65,25 @@ if (isset($submit)) {
 
         opener.parent.frames['code'].location.reload();
         opener.parent.frames['text'].location.replace('main.php?id_projeto=<?= $_SESSION['id_projeto_corrente'] ?>');
-    //self.close();
-    //location.href = "http://<?php print( CELConfig_ReadVar("HTTPD_ip") . "/" . CELConfig_ReadVar("CEL_dir_relativo")); ?>add_cenario.php?id_projeto=<?= $id_project ?>&sucesso=s" ;
+    <?php print( CELConfig_ReadVar("HTTPD_ip") . "/" . CELConfig_ReadVar("CEL_dir_relativo")); ?>add_cenario.php?id_projeto =<?= $id_project ?> & sucesso = s" ;
 
 
-        location.href = "add_cenario.php?id_projeto=<?= $id_project ?>&sucesso=s";
+                location.href = "add_cenario.php?id_projeto=<?= $id_project ?>&sucesso=s";
 
     </script>
 
     <?php
-} else {    // Script chamado atraves do menu superior
+} else {    // script called through the superior menu
     $project_name = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
     ?>
 
     <html>
         <head>
-            <title>Adicionar Cen�rio</title>
+            <title>Add scenario</title>
         </head>
         <body>
             <script language="JavaScript">
-            <!--
+                <!--
                 function checks_textArea(form)
                 {
                     title_area = form.titulo.value;
@@ -87,7 +92,7 @@ if (isset($submit)) {
 
                     if ((title_area == ""))
                     {
-                        alert("Por favor, digite o titulo do cen�rio.")
+                        alert("Please, insert the title of the scenario.")
                         form.titulo.focus()
                         return false;
                     } else {
@@ -95,7 +100,7 @@ if (isset($submit)) {
                         OK = padrao.exec(title_area);
                         if (OK)
                         {
-                            window.alert("O t�tulo do cen�rio n�o pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
+                            window.alert("The title of the scneario must not contain any of the follwing characters:   / \\ : ? \" < > |");
                             form.titulo.focus();
                             return false;
                         }
@@ -103,83 +108,83 @@ if (isset($submit)) {
 
                     if ((goal_area == ""))
                     {
-                        alert("Por favor, digite o objetivo do cen�rio.")
+                        alert("Please, insert the objective of the scenario.")
                         form.objetivo.focus()
                         return false;
                     }
 
                     if ((context_area == ""))
                     {
-                        alert("Por favor, digite o contexto do cen�rio.")
+                        alert("Please, insert the context of the scenario.")
                         form.contexto.focus()
                         return false;
                     }
                 }
-            //-->
+                //-->
 
     <?php
-    // Cen�rio -  Incluir Cen�rio 
-    //Objetivo:        Permitir ao usu�rio a inclus�o de um novo cen�rio
-    //Contexto:        Usu�rio deseja incluir um novo cen�rio.
-    //              Pr�-Condi��o: Login, cen�rio ainda n�o cadastrado
-    //Atores:        Usu�rio, Sistema
-    //Recursos:        Dados a serem cadastrados
-    //Epis�dios:    O sistema fornecer� para o usu�rio uma tela com os seguintes campos de texto:
-    //                - Nome Cen�rio
-    //                - Objetivo.  Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
-    //                - Contexto.  Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
-    //                - Atores.    Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
-    //                - Recursos.  Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
-    //                - Exce��o.   Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
-    //                - Epis�dios. Restri��o: Caixa de texto com pelo menos 16 linhas de escrita vis�veis
-    //                - Bot�o para confirmar a inclus�o do novo cen�rio
-    //              Restri��es: Depois de clicar no bot�o de confirma��o,
-    //                          o sistema verifica se todos os campos foram preenchidos. 
-    // Exce��o:        Se todos os campos n�o foram preenchidos, retorna para o usu�rio uma mensagem avisando
-    //              que todos os campos devem ser preenchidos e um bot�o de voltar para a pagina anterior.
+// Cen�rio -  Incluir Cen�rio 
+//Objetivo:        Permitir ao usu�rio a inclus�o de um novo cen�rio
+//Contexto:        Usu�rio deseja incluir um novo cen�rio.
+//              Pr�-Condi��o: Login, cen�rio ainda n�o cadastrado
+//Atores:        Usu�rio, Sistema
+//Recursos:        Dados a serem cadastrados
+//Epis�dios:    O sistema fornecer� para o usu�rio uma tela com os seguintes campos de texto:
+//                - Nome Cen�rio
+//                - Objetivo.  Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
+//                - Contexto.  Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
+//                - Atores.    Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
+//                - Recursos.  Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
+//                - Exce��o.   Restri��o: Caixa de texto com pelo menos 5 linhas de escrita vis�veis
+//                - Epis�dios. Restri��o: Caixa de texto com pelo menos 16 linhas de escrita vis�veis
+//                - Bot�o para confirmar a inclus�o do novo cen�rio
+//              Restri��es: Depois de clicar no bot�o de confirma��o,
+//                          o sistema verifica se todos os campos foram preenchidos. 
+// Exce��o:        Se todos os campos n�o foram preenchidos, retorna para o usu�rio uma mensagem avisando
+//              que todos os campos devem ser preenchidos e um bot�o de voltar para a pagina anterior.
     ?>
 
             </SCRIPT>
 
-            <h4>Adicionar Cen�rio</h4>
+            <h4>Add scenario</h4>
             <br>
-    <?php
-    if ($success == "s") {
-        ?>
-                <p style="color: blue; font-weight: bold; text-align: center">Cen�rio inserido com sucesso!</p>
-        <?php
-    }
-    ?>    
+            <?php
+            if ($success == "s") {
+                ?>
+                <p style="color: blue; font-weight: bold; text-align: center">Scenario inserted with success!</p>
+                <?php
+            }
+            ?>    
             <form action="" method="post">
                 <table>
                     <tr>
-                        <td>Projeto:</td>
+                        <td>Project:</td>
                         <td><input disabled size="51" type="text" value="<?= $project_name ?>"></td>
                     </tr>
-                    <td>T�tulo:</td>
+                    <td>Title:</td>
                     <td><input size="51" name="titulo" type="text" value=""></td>                
                     <tr>
-                        <td>Objetivo:</td>
+                        <td>Objective:</td>
                         <td><textarea cols="51" name="objetivo" rows="3" WRAP="SOFT"></textarea></td>
                     </tr>
                     <tr>
-                        <td>Contexto:</td>
+                        <td>Context:</td>
                         <td><textarea cols="51" name="contexto" rows="3" WRAP="SOFT"></textarea></td>
                     </tr>
                     <tr>
-                        <td>Atores:</td>
+                        <td>Actors:</td>
                         <td><textarea cols="51" name="atores" rows="3" WRAP="SOFT"></textarea></td>
                     </tr>
                     <tr>
-                        <td>Recursos:</td>
+                        <td>Resources:</td>
                         <td><textarea cols="51" name="recursos" rows="3" WRAP="SOFT"></textarea></td>
                     </tr>
                     <tr>
-                        <td>Exce��o:</td>
+                        <td>Exception:</td>
                         <td><textarea cols="51" name="excecao" rows="3" WRAP="SOFT"></textarea></td>
                     </tr>
                     <tr>
-                        <td>Epis�dios:</td>
+                        <td>Episodes:</td>
                         <td><textarea cols="51" name="episodios" rows="5" WRAP="SOFT"></textarea></td>
                     </tr>
                     <tr>
@@ -187,8 +192,8 @@ if (isset($submit)) {
                     </tr>
                 </table>
             </form>
-        <center><a href="javascript:self.close();">Fechar</a></center>
-        <br><i><a href="showSource.php?file=add_cenario.php">Veja o c�digo fonte!</a></i>
+        <center><a href="javascript:self.close();">Close</a></center>
+        <br><i><a href="showSource.php?file=add_cenario.php">See the source code!</a></i>
     </body>
     </html>
 
