5f89cfe0fc8e6b8a501f11db342b4030b409ee5f
paulohtfs <paulohtfs@gmail.com>
2013-09-28 15:04:23 -0300

    Refactor "login.php" file

--- cel/aplicacao/login.php
+++ cel/aplicacao/login.php
@@ -4,18 +4,6 @@
  * Purpose: View of the login screen with their scenarios
  */
 
-/**
-  @Titulo: Acessar o sistema
-
-  @Objetivo: Permitir que o usu�rio acesse a Aplica��o de Edi��o de L�xicos e de Edi��o de Cen�rios, cadastre-se no sistema ou requisite sua senha no caso de t�-la esquecido.
-
-  @Contexto: A p�gina da aplica��o � acessada. Na p�gina de abertura ../cel/aplicacao/login.php o usu�rio insere login ou senha incorretos - $wrong=true.
-
-  @Atores: usu�rio, aplica��o
-
-  @Recursos: URL de acesso ao sistema,  login, senha, bd.inc, httprequest.inc, $wrong, $url, showSource.php?file=login.php, forgotten_password.php, add_usuario.php?novo=true
- * */
-/** @Episodio 1: Iniciar sess�o * */
 define("SELECT_USER", "SELECT * FROM usuario WHERE login='%s' AND senha='%s'");
 
 session_start();
@@ -25,11 +13,12 @@ include("httprequest.inc");
 
 $url = '';
 $submit = false;
+$first_access = true;
 $authenticated = false;
 $current_userLogin = "NAME";
 $current_userPassword = "PASSWORD";
 
-function shows_loginForm() {
+function shows_loginForm($first_access) {
     ?>
 
     <html>
@@ -39,49 +28,41 @@ function shows_loginForm() {
         <body>
 
             <?php
-            /** @Episodio 4: Se wrong = true ent�o mostrar a mensagem Login ou Senha incorreto . */
-            if (true) {
-                ?>
-
-                <p style="color: red; font-weight: bold; text-align: center">
-                    <img src="Images/Logo_CEL.jpg" width="180" height="180"><br/><br/>
-                    &nbsp;&nbsp;&nbsp;&nbsp;Login ou Senha Incorreto</p>
-
-                <?php
-            } else {/** @Episodio 5: Se wrong != true ent�o mostrar a mensagem Entre com seu login e senha. */
+            if ($first_access) {
                 ?>
                 <p style="color: green; font-weight: bold; text-align: center">
                     <img src="Images/Logo_CEL.jpg" width="100" height="100"><br/><br/>
                     &nbsp;&nbsp;&nbsp;&nbsp;Entre com seu Login e Senha:</p>
-
-                }
+                <?php
+            } else {
                 ?>
-
-                <form action="?url=<?= $url ?>" method="post">
-                    <div align="center">
-                        <table cellpadding="5">
-                            <tr><td>Login:</td><td><input maxlength="32" name="login" size="24" type="text"></td></tr>
-                            <tr><td>Senha:</td><td><input maxlength="32" name="senha" size="24" type="password"></td></tr>
-                            <tr><td height="10"></td></tr>
-                            <tr><td align="center" colspan="2"><input name="submit" type="submit" value="Entrar"></td></tr>
-                        </table>
-
-                        <p><a href="adds_user.php">Cadastrar-se</a>&nbsp;&nbsp;
-
-                            <a href="forgotten_password.php">Esqueci senha</a></p>
-                    </div>
-                </form>
-            </body>
-
-            <?php
-            /** @Episodio 8: [MOSTRAR O C�DIGO FONTE]  */
+                <p style="color: red; font-weight: bold; text-align: center">
+                    <img src="Images/Logo_CEL.jpg" width="180" height="180"><br/><br/>
+                    &nbsp;&nbsp;&nbsp;&nbsp;Login ou Senha Incorreto</p>
+                <?php
+            }
             ?>
 
-            <i><a href="showSource.php?file=login.php">Veja o c�digo fonte!</a></i>
-        </html>
+            <form action="login.php" method="POST">
+                <div align="center">
+                    <table cellpadding="5">
+                        <tr><td>Login:</td><td><input maxlength="32" name="login" size="24" type="text"></td></tr>
+                        <tr><td>Senha:</td><td><input maxlength="32" name="senha" size="24" type="password"></td></tr>
+                        <tr><td height="10"></td></tr>
+                        <tr><td align="center" colspan="2"><input name="submit" type="submit" value="Entrar"></td></tr>
+                    </table>
 
-        <?php
-    }
+                    <p><a href="adds_user.php">Cadastrar-se</a>&nbsp;&nbsp;
+
+                        <a href="forgotten_password.php">Esqueci senha</a></p>
+                </div>
+            </form>
+        </body>
+
+        <i><a href="showSource.php?file=login.php">Veja o c�digo fonte!</a></i>
+    </html>
+
+    <?php
 }
 
 function authenticate_user($userName, $userPassword) {
@@ -103,11 +84,10 @@ function authenticate_user($userName, $userPassword) {
 
 /** @Episodio 2: Conectar o SGBD * */
 /** @Restri��o: a fun��o bd_connect definida em bd.inc � utilizada * */
-/** @Exce��o: Erro ao conectar banco de dados * */
-//$database_conection = database_connect() or die("Error while connecting to SGBD");
+/** @Exce��o: Erro ao conectar banco de dados **/
 
 /** @Episodio 9: Se o formul�rio tiver sido submetido ent�o verificar se o login e senha est�o corretos. * */
-if ($submit == 'Entrar') {
+if ($submit) {
     $senha_cript = md5($user_password);
     $query = "SELECT id_usuario FROM usuario WHERE login='$user_login' AND senha='$senha_cript'";
     $query_r = mysql_query($query) or die("Erro ao executar a query");
