3e8c107d7b500cce2cfd13554489b99aec8338db
paulohtfs <paulohtfs@gmail.com>
2013-10-18 10:56:38 -0300

    Created project_function_test

---
+++ Teste/cel/aplicacao/project_function_test.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../../cel/aplicacao/project_function.php';
+
+class test_project_function extends PHPUnit_Framework_TestCase{
+    
+    function test_inclui_projeto(){
+        $result = inclui_projeto("NOME1", "DESC");
+        $this->assertEquals(1,$result);
+    }
+    
+}
+?>
