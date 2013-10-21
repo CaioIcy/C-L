870c35987109fceec43e3e1e388691a17b4f8214
ricardogtx <ricardocdzg@hotmail.com>
2013-10-21 20:57:45 -0200

    start test

--- Teste/cel/aplicacao/ScenarioFunctionTest.php
+++ Teste/cel/aplicacao/ScenarioFunctionTest.php
@@ -1,14 +1,20 @@
 <?php
 require_once dirname(__FILE__) . '/../../../cel/aplicacao/ScenarioFunctions.php';
+require_once dirname(__FILE__) . '/../../../cel/aplicacao/ProjectFunction.php';
 
 class test_scenario_function extends PHPUnit_Framework_TestCase{
     
-    private $id_project=0;
- 
+      private $id_project=NULL;
     
-    public function test_inclui_cenario() {
-        $result = inclui_cenario($this->id_project);
-        $this->assertNotNull($result);
+    protected function setUp() {
+        $_SESSION['id_current_user'] = "nivi";
+        $this->id_project = includeProject("NOME2", "DESC2");
     }
+    
+    public function testIncluiCenario() {
+        $result = inclui_cenario($this->id_project);
+        $this->assertEmpty($result);
+    }
+    
 }
 ?>
