3f0445d7db01a17e66ba55552f676c01c8efa5b5
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2013-10-23 23:15:37 -0200

    emoving 'checarCenarioExistente' to ScenarioFunctions.php

--- cel/aplicacao/ScenarioFunctions.php
+++ cel/aplicacao/ScenarioFunctions.php
@@ -425,4 +425,27 @@ if (!(function_exists("inserirPedidoRemoverCenario"))) {
     }
 
 }
+###################################################################
+# Funcao faz um select na tabela cenario.
+# Para inserir um novo cenario, deve ser verificado se ele ja existe.
+# Recebe o id do projeto e o titulo do cenario (1.0)
+# Faz um SELECT na tabela cenario procurando por um nome semelhante
+# no projeto (1.2)
+# retorna true caso nao exista ou false caso exista (1.3)
+###################################################################
+
+function checarCenarioExistente($projeto, $titulo) {
+    $naoexiste = false;
+
+    $r = bd_connect() or die("Erro ao conectar ao SGBD<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+    $q = "SELECT * FROM cenario WHERE id_projeto = $projeto AND titulo = '$titulo' ";
+    $qr = mysql_query($q) or die("Erro ao enviar a query de select no cenario<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+    $resultArray = mysql_fetch_array($qr);
+    if ($resultArray == false) {
+        $naoexiste = true;
+    }
+
+    return $naoexiste;
+}
+
 ?>
