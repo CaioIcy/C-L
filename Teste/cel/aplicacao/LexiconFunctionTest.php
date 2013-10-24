122e367ae10d00bce8bece7f2ed87a8583b29fd0
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2013-10-23 23:32:04 -0200

    teste de incluir_lexico

--- Teste/cel/aplicacao/LexiconFunctionTest.php
+++ Teste/cel/aplicacao/LexiconFunctionTest.php
@@ -1,7 +1,24 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once dirname(__FILE__) . '/../../../cel/aplicacao/ScenarioFunctions.php';
+require_once dirname(__FILE__) . '/../../../cel/aplicacao/ProjectFunction.php';
+require_once dirname(__FILE__) . '/../../../cel/aplicacao/LexiconFunctions.php';
+
+class test_lexicon_function extends PHPUnit_Framework_TestCase{
+ 
+     protected function setUp() {
+        includeProject("NOME2", "DESC2");
+    }
+
+    function testIncluiLexico() {
+        $result = inclui_lexico("NOME2", "DESC2");
+        $this->assertFalse($result);
+    }
+
+    function testRemoveProject() {
+        $result = removeProject("2");
+        $this->assertTrue($result);
+    }
+    
+}
 ?>
