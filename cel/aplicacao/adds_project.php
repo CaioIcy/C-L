00384df32cfaab986e77cbb5c1f9e4b0be49562c
paulohtfs <paulohtfs@gmail.com>
2013-10-27 14:11:12 -0200

    Fixing adds project

--- cel/aplicacao/adds_project.php
+++ cel/aplicacao/adds_project.php
@@ -1,137 +1,119 @@
 <?php
-/* File: adds_project.php
- * Purpose: This script registers a new project
- */
-session_start();
 
-include_once 'funcoes_genericas.php';
-include_once 'httprequest.inc';
+session_start(); 
 
-// Access control scenario
-check_user_authentication("index.php");
+include("funcoes_genericas.php");
+include("httprequest.inc");
 
-/*
- * This script is called when there is an inclusion solicitation of a new
- * project, or when a new user registers in the system
- */
+// Cenário controle de acesso
+check_user_authentication("index.php");        
 
-/*
-//Cen�rio  -  Cadastrar Novo Projeto 
-//Objetivo:	   Permitir ao usu�rio cadastrar um novo projeto
-//Contexto:	   Usu�rio deseja incluir um novo projeto na base de dados
-//              Pr�-Condi��o: Login  
-//Atores:	   Usu�rio
-//Recursos:	   Sistema, dados do projeto, base de dados
-//Epis�dios:   O Usu�rio clica na op��o �adicionar projeto� encontrada no menu superior.
-//             O sistema disponibiliza uma tela para o usu�rio especificar os dados do novo projeto,
-//              como o nome do projeto e sua descri��o.
-//             O usu�rio clica no bot�o inserir.
-//             O sistema grava o novo projeto na base de dados e automaticamente constr�i a Navega��o
+// Este script eh chamado quando ocorre uma solicitacao de inclusao
+// de novo projeto, ou quando um novo usuario se cadastra no sistema
+
+
+//Cenário  -  Cadastrar Novo Projeto 
+//Objetivo:           Permitir ao usuário cadastrar um novo projeto
+//Contexto:           Usuário deseja incluir um novo projeto na base de dados
+//              Pré-Condição: Login  
+//Atores:           Usuário
+//Recursos:           Sistema, dados do projeto, base de dados
+//Episódios:   O Usuário clica na opção “adicionar projeto” encontrada no menu superior.
+//             O sistema disponibiliza uma tela para o usuário especificar os dados do novo projeto,
+//              como o nome do projeto e sua descrição.
+//             O usuário clica no botão inserir.
+//             O sistema grava o novo projeto na base de dados e automaticamente constrói a Navegação
 //              para este novo projeto.
-//Exce��o:	   Se for especificado um nome de projeto j� existente e que perten�a ou tenha a participa��o
-//                 deste usu�rio, o sistema exibe uma mensagem de erro.
-// Called through the submit button
-*/
+//Exceção:           Se for especificado um nome de projeto já existente e que pertença ou tenha a participação
+//                 deste usuário, o sistema exibe uma mensagem de erro.
 
-function include_inDatabase($project_name,$project_description){
-    
-    $database = database_connect();
-    
-    $id_currentUser = $_SESSION['id_currentUser'];
-    $id_includedProject = inclui_projeto($project_name, $project_description);
-    $manager = 1;
-    
-    $query_project = "INSERT INTO participa (id_user, id_project, manager) VALUES ($id_currentUser,$id_includedProject, $manager)";
-    mysql_query($query_project) or die("ERROR while connection to the main table");
-    
-}
+// Chamado atraves do botao de submit
+if (isset($submit)) {    
 
-if (isset($submit)) {
+        $id_projeto_incluido = inclui_projeto($nome, $descricao);
     
+        // Inserir na tabela participa
     
-    $id_includedProject = inclui_projeto($project_name, $project_description);
-
-    // Insert in the main table
-
-    if ($id_includedProject != -1) {
-        
-        $database_conection = database_connect() or die("Error while connecting to SGBD");
-        $manager = 1;
-        $id_currentUser = $_SESSION['id_usuario_corrente'];
-        $query_database_command = "INSERT INTO participa (id_user, id_project, manager) VALUES ($id_currentUser, $id_includedProject, $manager)";
-        mysql_query($query_database_command) or die("Error while connection to the main table");
-        
-    } else {
-        ?>
-        <html>
-            <title>Error</title>
-            <body>
-                <p style="color: red; font-weight: bold; text-align: center">Project name already exists!</p>
-            <center><a href="JavaScript:window.history.go(-1)">Return</a></center>
-        </body>
-        </html>   
-        <?php
-        return;
+        if ($id_projeto_incluido != -1 )
+    {
+            $r = database_connect() or die("Erro ao conectar ao SGBD");
+            $gerente = 1;
+            $id_usuario_corrente = $_SESSION['id_usuario_corrente'];    
+            $q = "INSERT INTO participa (id_usuario, id_projeto, gerente) VALUES ($id_usuario_corrente, $id_projeto_incluido, $gerente  )";
+            mysql_query($q) or die("Erro ao inserir na tabela participa");
     }
-    ?>
+    else
+    {
+ ?>
+ <html>
+ <title>Erro</title>
+ <body>
+    <p style="color: red; font-weight: bold; text-align: center">Nome de projeto já existente!</p>
+    <center><a href="JavaScript:window.history.go(-1)">Voltar</a></center>
+ </body>
+ </html>   
+ <?php          
+            return;
+    }            
+?>
 
-    <script language="javascript1.3">
+<script language="javascript1.3">
 
-        self.close();
+self.close();
 
-    </script>
+</script>
 
-    <?php
-// Called normally
-} else {
-    ?>
+<?php
+// Chamado normalmente
+} else {        
+?>
 
-    <html>
-        <head>
-            <title>Add project</title>
-            <script language="javascript1.3">
+<html>
+    <head>
+        <title>Adicionar Projeto</title>
+        <script language="javascript1.3">
 
-                function checks_form_values() {
-                    if (document.forms[0].nome.value == "") {
-                        alert('Fill the field "Name"');
-                        document.forms[0].nome.focus();
-                        return false;
-                    } else {
-                        padrao = /[\\\/\?"<>:|]/;
-                        nOK = padrao.exec(document.forms[0].nome.value);
-                        if (nOK)
-                        {
-                            window.alert("The name of the project must not contain any of the follwoing characters:   / \\ : ? \" < > |");
-                            document.forms[0].nome.focus();
-                            return false;
+        function chkFrmVals() {
+            if (document.forms[0].nome.value == "") {
+                alert('Preencha o campo "Nome"');
+                                document.forms[0].nome.focus();
+                return false;
+            }else{
+                                padrao = /[\\\/\?"<>:|]/;
+                                nOK = padrao.exec(document.forms[0].nome.value);
+                                if (nOK)
+                                {
+                                        window.alert ("O nome do projeto não pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
+                                        document.forms[0].nome.focus();
+                                        return false;
+                                }         
                         }
-                    }
-                    return true;
-                }
+            return true;
+        }
 
-            </script>
-        </head>
-        <body>
-            <h4>Add project:</h4>
-            <br>
-            <form action="" method="post" onSubmit="return checks_form_values();">
-                <table>
-                    <tr>
-                        <td>Name:</td>
-                        <td><input maxlength="128" name="nome" size="48" type="text"></td>
-                    </tr>
-                    <tr>
-                        <td>Description:</td>
-                        <td><textarea cols="48" name="descricao" rows="4"></textarea></td>
-                    <tr>
-                        <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Adicionar Projeto"></td>
-                    </tr>
-                </table>
-            </form>
-            <br><i><a href="showSource.php?file=add_projeto.php">See the source code!</a></i>
-        </body>
-    </html>
+        </script>
+    </head>
+    <body>
+        <h4>Adicionar Projeto:</h4>
+        <br>
+        <form action="" method="post" onSubmit="return chkFrmVals();">
+        <table>
+            <tr>
+                <td>Nome:</td>
+                <td><input maxlength="128" name="nome" size="48" type="text"></td>
+            </tr>
+            <tr>
+                <td>Descrição:</td>
+                <td><textarea cols="48" name="descricao" rows="4"></textarea></td>
+            <tr>
+                <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Adicionar Projeto"></td>
+            </tr>
+        </table>
+        </form>
+        <br><i><a href="showSource.php?file=adds_project.php">Veja o código fonte!</a></i>
+    </body>
+</html>
 
-    <?php
+<?php
 }
 ?>
