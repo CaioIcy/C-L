4fc25e1deb6980154b3a49481fd92cc3485cf61f
paulohtfs <paulohtfs@gmail.com>
2013-10-29 11:01:53 -0200

    Fixed adds project

--- cel/aplicacao/adds_project.php
+++ cel/aplicacao/adds_project.php
@@ -1,17 +1,14 @@
 <?php
-
-session_start(); 
+session_start();
 
 include("funcoes_genericas.php");
 include("httprequest.inc");
 
 // Cenário controle de acesso
-check_user_authentication("index.php");        
+check_user_authentication("index.php");
 
 // Este script eh chamado quando ocorre uma solicitacao de inclusao
 // de novo projeto, ou quando um novo usuario se cadastra no sistema
-
-
 //Cenário  -  Cadastrar Novo Projeto 
 //Objetivo:           Permitir ao usuário cadastrar um novo projeto
 //Contexto:           Usuário deseja incluir um novo projeto na base de dados
@@ -26,94 +23,93 @@ check_user_authentication("index.php");
 //              para este novo projeto.
 //Exceção:           Se for especificado um nome de projeto já existente e que pertença ou tenha a participação
 //                 deste usuário, o sistema exibe uma mensagem de erro.
-
 // Chamado atraves do botao de submit
-if (isset($submit)) {    
+if (isset($submit)) {
+    
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
 
-        $id_projeto_incluido = inclui_projeto($nome, $descricao);
-    
-        // Inserir na tabela participa
-    
-        if ($id_projeto_incluido != -1 )
-    {
-            $r = database_connect() or die("Erro ao conectar ao SGBD");
-            $gerente = 1;
-            $id_usuario_corrente = $_SESSION['id_usuario_corrente'];    
-            $q = "INSERT INTO participa (id_usuario, id_projeto, gerente) VALUES ($id_usuario_corrente, $id_projeto_incluido, $gerente  )";
-            mysql_query($q) or die("Erro ao inserir na tabela participa");
+    $id_projeto_incluido = inclui_projeto($nome, $descricao);
+
+    // Inserir na tabela participa
+
+    if ($id_projeto_incluido != -1) {
+        $r = database_connect() or die("Erro ao conectar ao SGBD");
+        $gerente = 1;
+        $id_usuario_corrente = $_SESSION['id_usuario_corrente'];
+        $q = "INSERT INTO participa (id_usuario, id_projeto, gerente) VALUES ($id_usuario_corrente, $id_projeto_incluido, $gerente  )";
+        mysql_query($q) or die("Erro ao inserir na tabela participa");
+    } else {
+        ?>
+        <html>
+            <title>Erro</title>
+            <body>
+                <p style="color: red; font-weight: bold; text-align: center">Nome de projeto já existente!</p>
+            <center><a href="JavaScript:window.history.go(-1)">Voltar</a></center>
+        </body>
+        </html>   
+        <?php
+        return;
     }
-    else
-    {
- ?>
- <html>
- <title>Erro</title>
- <body>
-    <p style="color: red; font-weight: bold; text-align: center">Nome de projeto já existente!</p>
-    <center><a href="JavaScript:window.history.go(-1)">Voltar</a></center>
- </body>
- </html>   
- <?php          
-            return;
-    }            
-?>
+    ?>
 
-<script language="javascript1.3">
+    <script language="javascript1.3">
 
-self.close();
+        self.close();
 
-</script>
+    </script>
 
-<?php
+    <?php
 // Chamado normalmente
-} else {        
-?>
+} else {
+    ?>
 
-<html>
-    <head>
-        <title>Adicionar Projeto</title>
-        <script language="javascript1.3">
+    <html>
+        <head>
+            <title>Adicionar Projeto</title>
+            <script language="javascript1.3">
 
-        function chkFrmVals() {
-            if (document.forms[0].nome.value == "") {
-                alert('Preencha o campo "Nome"');
-                                document.forms[0].nome.focus();
-                return false;
-            }else{
-                                padrao = /[\\\/\?"<>:|]/;
-                                nOK = padrao.exec(document.forms[0].nome.value);
-                                if (nOK)
-                                {
-                                        window.alert ("O nome do projeto não pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
-                                        document.forms[0].nome.focus();
-                                        return false;
-                                }         
+                function chkFrmVals() {
+                    if (document.forms[0].nome.value == "") {
+                        alert('Preencha o campo "Nome"');
+                        document.forms[0].nome.focus();
+                        return false;
+                    } else {
+                        padrao = /[\\\/\?"<>:|]/;
+                        nOK = padrao.exec(document.forms[0].nome.value);
+                        if (nOK)
+                        {
+                            window.alert("O nome do projeto não pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
+                            document.forms[0].nome.focus();
+                            return false;
                         }
-            return true;
-        }
+                    }
+                    return true;
+                }
 
-        </script>
-    </head>
-    <body>
-        <h4>Adicionar Projeto:</h4>
-        <br>
-        <form action="" method="post" onSubmit="return chkFrmVals();">
-        <table>
-            <tr>
-                <td>Nome:</td>
-                <td><input maxlength="128" name="nome" size="48" type="text"></td>
-            </tr>
-            <tr>
-                <td>Descrição:</td>
-                <td><textarea cols="48" name="descricao" rows="4"></textarea></td>
-            <tr>
-                <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Adicionar Projeto"></td>
-            </tr>
-        </table>
-        </form>
-        <br><i><a href="showSource.php?file=adds_project.php">Veja o código fonte!</a></i>
-    </body>
-</html>
+            </script>
+        </head>
+        <body>
+            <h4>Adicionar Projeto:</h4>
+            <br>
+            <form action="" method="post" onSubmit="return chkFrmVals();">
+                <table>
+                    <tr>
+                        <td>Nome:</td>
+                        <td><input maxlength="128" name="nome" size="48" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>Descrição:</td>
+                        <td><textarea cols="48" name="descricao" rows="4"></textarea></td>
+                    <tr>
+                        <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Adicionar Projeto"></td>
+                    </tr>
+                </table>
+            </form>
+            <br><i><a href="showSource.php?file=adds_project.php">Veja o código fonte!</a></i>
+        </body>
+    </html>
 
-<?php
+    <?php
 }
 ?>
