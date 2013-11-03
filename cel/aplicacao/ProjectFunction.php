2c7cb06d569d659b62f4bfa0b898bad81b0d468c
paulohtfs <paulohtfs@gmail.com>
2013-11-03 10:24:37 -0200

    Revert ProjectFunction

--- cel/aplicacao/ProjectFunction.php
+++ cel/aplicacao/ProjectFunction.php
@@ -1,30 +1,41 @@
 <?php
+
 /**
  * File name: ProjectFunction.php
  * Propuse: Function related to project
  */
-
 require_once 'Dao/DaoProject.php';
 require_once 'seguranca.php';
 
 /*
- *Insere um projeto no banco de dados.
- *Recebe o nome e descricao. (1.1)
- *Verifica se este usuario ja possui um projeto com esse nome. (1.2)
- *Caso nao possua, insere os valores na tabela PROJETO. (1.3)
- *Devolve o id_cprojeto. (1.4)
+ * Insere um projeto no banco de dados.
+ * Recebe o nome e descricao. (1.1)
+ * Verifica se este usuario ja possui um projeto com esse nome. (1.2)
+ * Caso nao possua, insere os valores na tabela PROJETO. (1.3)
+ * Devolve o id_cprojeto. (1.4)
  */
 
-function includeProject($projectName, $projectDescription) {
-    
-    $projectArray = getProjectNameDatabase($projectName);
+function inclui_projeto($nome, $descricao) {
+    $r = database_connect() or die("Erro ao conectar ao SGBD<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+    //verifica se usuario ja existe
+    $qv = "SELECT * FROM projeto WHERE nome = '$nome'";
+    $qvr = mysql_query($qv) or die("Erro ao enviar a query de select<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
 
-    if ($projectArray != false) {
+    //$result = mysql_fetch_row($qvr);
+    $resultArray = mysql_fetch_array($qvr);
+
+
+    if ($resultArray != false) {
         //verifica se o nome existente corresponde a um projeto que este usuario participa
-        $id_projeto_repetido = $projectArray['id_projeto'];
-        $idCurrentUser = $_SESSION['id_usuario_corrente'];
-        
-        $resultArray = getProjectIdDatabase($id_projeto_repetido, $idCurrentUser);
+        $id_projeto_repetido = $resultArray['id_projeto'];
+
+        $id_usuario_corrente = $_SESSION['id_usuario_corrente'];
+
+        $qvu = "SELECT * FROM participa WHERE id_projeto = '$id_projeto_repetido' AND id_usuario = '$id_usuario_corrente' ";
+
+        $qvuv = mysql_query($qvu) or die("Erro ao enviar a query de SELECT no participa<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+        $resultArray = mysql_fetch_row($qvuv);
 
         if ($resultArray[0] != null) {
             return -1;
@@ -43,7 +54,7 @@ function includeProject($projectName, $projectDescription) {
     $data = date("Y-m-d");
 
     $qr = "INSERT INTO projeto (id_projeto, nome, data_criacao, descricao)
-                  VALUES ($result[0],'" . prepara_dado($nome) . "','$data' , '" . prepara_dado($projectDescription) . "')";
+                  VALUES ($result[0],'" . prepara_dado($nome) . "','$data' , '" . prepara_dado($descricao) . "')";
 
     mysql_query($qr) or die("Erro ao enviar a query INSERT<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
 
@@ -70,17 +81,69 @@ function includeProject($projectName, $projectDescription) {
 #
 ###################################################################
 
-function removeProject($projectId) {
-    
-    $result = FALSE;
-    $rmvRresult = rmvProjectDatabase($projectId);
-    
-    if($rmvRresult==1){
-        $result = TRUE;
-    } else {
-        $result = FALSE;
+function removeProjeto($id_projeto) {
+    $r = database_connect() or die("Erro ao conectar ao SGBD<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+    //Remove os pedidos de cenario
+    $qv = "Delete FROM pedidocen WHERE id_projeto = '$id_projeto' ";
+    $deletaPedidoCenario = mysql_query($qv) or die("Erro ao apagar pedidos de cenario<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+    //Remove os pedidos de lexico
+    $qv = "Delete FROM pedidolex WHERE id_projeto = '$id_projeto' ";
+    $deletaPedidoLexico = mysql_query($qv) or die("Erro ao apagar pedidos do lexico<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+    //Remove os lexicos //verificar lextolex!!!
+    $qv = "SELECT * FROM lexico WHERE id_projeto = '$id_projeto' ";
+    $qvr = mysql_query($qv) or die("Erro ao enviar a query de select no lexico<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+    while ($result = mysql_fetch_array($qvr)) {
+        $id_lexico = $result['id_lexico']; //seleciona um lexico
+
+        $qv = "Delete FROM lextolex WHERE id_lexico_from = '$id_lexico' OR id_lexico_to = '$id_lexico' ";
+        $deletaLextoLe = mysql_query($qv) or die("Erro ao apagar pedidos do lextolex<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+        $qv = "Delete FROM centolex WHERE id_lexico = '$id_lexico'";
+        $deletacentolex = mysql_query($qv) or die("Erro ao apagar pedidos do centolex<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+        //$qv = "Delete FROM sinonimo WHERE id_lexico = '$id_lexico'";
+        //$deletacentolex = mysql_query($qv) or die("Erro ao apagar sinonimo<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+        $qv = "Delete FROM sinonimo WHERE id_projeto = '$id_projeto'";
+        $deletacentolex = mysql_query($qv) or die("Erro ao apagar sinonimo<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
     }
-    
-    return $result;
+
+    $qv = "Delete FROM lexico WHERE id_projeto = '$id_projeto' ";
+    $deletaLexico = mysql_query($qv) or die("Erro ao apagar pedidos do lexico<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+    //remove os cenarios
+    $qv = "SELECT * FROM cenario WHERE id_projeto = '$id_projeto' ";
+    $qvr = mysql_query($qv) or die("Erro ao enviar a query de select no cenario<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+    $resultArrayCenario = mysql_fetch_array($qvr);
+
+    while ($result = mysql_fetch_array($qvr)) {
+        $id_lexico = $result['id_cenario']; //seleciona um lexico
+
+        $qv = "Delete FROM centocen WHERE id_cenario_from = '$id_cenario' OR id_cenario_to = '$id_cenario' ";
+        $deletaCentoCen = mysql_query($qv) or die("Erro ao apagar pedidos do centocen<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+        $qv = "Delete FROM centolex WHERE id_cenario = '$id_cenario'";
+        $deletaLextoLe = mysql_query($qv) or die("Erro ao apagar pedidos do centolex<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+    }
+
+    $qv = "Delete FROM cenario WHERE id_projeto = '$id_projeto' ";
+    $deletaLexico = mysql_query($qv) or die("Erro ao apagar pedidos do cenario<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+    //remover participantes
+    $qv = "Delete FROM participa WHERE id_projeto = '$id_projeto' ";
+    $deletaParticipantes = mysql_query($qv) or die("Erro ao apagar no participa<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+    //remover publicacao
+    $qv = "Delete FROM publicacao WHERE id_projeto = '$id_projeto' ";
+    $deletaPublicacao = mysql_query($qv) or die("Erro ao apagar no publicacao<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+
+    //remover projeto
+    $qv = "Delete FROM projeto WHERE id_projeto = '$id_projeto' ";
+    $deletaProjeto = mysql_query($qv) or die("Erro ao apagar no participa<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
 }
+
 ?>
