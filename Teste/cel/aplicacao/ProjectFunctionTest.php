d928cff03020fd7722d8039e83cf13cd749496b2
paulohtfs <paulohtfs@gmail.com>
2013-11-03 15:45:10 -0200

    Test evolution

--- Teste/cel/aplicacao/ProjectFunctionTest.php
+++ Teste/cel/aplicacao/ProjectFunctionTest.php
@@ -4,13 +4,19 @@ require_once dirname(__FILE__) . '/../../../cel/aplicacao/ProjectFunction.php';
 
 class test_project_function extends PHPUnit_Framework_TestCase {
 
-    protected function setUp() {
-        includeProject("NOME2", "DESC2");
+    protected function setUpProjectInDatabase() {
+        includeProject("NOME1", "DESC2");
+    }
+
+    protected function tearDownProjectInDatabase() {
+        removeProjeto("NOME1");
     }
 
     function testIncluiProjeto() {
+        $this->setUpProjectInDatabase();
         $result = inclui_projeto("NOME2", "DESC2");
         $this->assertFalse($result);
+        $this->tearDownProjectInDatabase();
     }
 
     function testRemoveProject() {
