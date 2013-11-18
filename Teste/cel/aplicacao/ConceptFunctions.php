2ef11254d7f2e598a406ba2d31858a080086002c
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2013-11-18 16:33:33 -0200

    creating class 'ConceptFunctions' from 'funcoes_genericas'

---
+++ Teste/cel/aplicacao/ConceptFunctions.php
@@ -0,0 +1,215 @@
+<?php
+
+include_once 'bd.inc';
+include_once 'seguranca.php';
+include_once 'class_database.php';
+
+###################################################################
+# Funcao faz um insert na tabela de pedido.
+# Para alterar um conceito ela deve receber os campos do conceito
+# jah modificados.(1.1)
+# Ao final ela manda um e-mail para o gerentes do projeto
+# referente a este cenario caso o criador n�o seja o gerente.(2.1)
+# Arquivos que utilizam essa funcao:
+# alt_cenario.php
+###################################################################
+if (!(function_exists("inserirPedidoAlterarCenario"))) {
+
+    function inserirPedidoAlterarConceito($id_projeto, $id_conceito, $nome, $descricao, $namespace, $justificativa, $id_usuario) {
+        $DB = new PGDB();
+        $insere = new QUERY($DB);
+        $select = new QUERY($DB);
+        $select2 = new QUERY($DB);
+
+        $q = "SELECT * FROM participa WHERE gerente = 1 AND id_usuario = $id_usuario AND id_projeto = $id_projeto";
+        $qr = mysql_query($q) or die("Erro ao enviar a query de select no participa<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+        $resultArray = mysql_fetch_array($qr);
+
+
+        if ($resultArray == false) { //nao e gerente
+            $insere->execute("INSERT INTO pedidocon (id_projeto, id_conceito, nome, descricao, namespace, id_usuario, tipo_pedido, aprovado, justificativa) VALUES ($id_projeto, $id_conceito, '$nome', '$descricao', '$namespace', $id_usuario, 'alterar', 0, '$justificativa')");
+            $select->execute("SELECT * FROM usuario WHERE id_usuario = $id_usuario");
+            $select2->execute("SELECT * FROM participa WHERE gerente = 1 AND id_projeto = $id_projeto");
+            $record = $select->gofirst();
+            $nomeUsuario = $record['nome'];
+            $email = $record['email'];
+            $record2 = $select2->gofirst();
+            while ($record2 != 'LAST_RECORD_REACHED') {
+                $id = $record2['id_usuario'];
+                $select->execute("SELECT * FROM usuario WHERE id_usuario = $id");
+                $record = $select->gofirst();
+                $mailGerente = $record['email'];
+                mail("$mailGerente", "Pedido de Altera��o Conceito", "O usuario do sistema $nomeUsuario\nPede para alterar o conceito $nome \nObrigado!", "From: $nomeUsuario\r\n" . "Reply-To: $email\r\n");
+                $record2 = $select2->gonext();
+            }
+        } else { //Eh gerente
+            removeConceito($id_projeto, $id_conceito);
+            adicionar_conceito($id_projeto, $nome, $descricao, $namespace);
+        }
+    }
+
+}
+
+###################################################################
+# Funcao faz um insert na tabela de pedido.
+# Para remover um conceito ela deve receber
+# o id do conceito e id projeto.(1.1)
+# Ao final ela manda um e-mail para o gerente do projeto
+# referente a este conceito.(2.1)
+# Arquivos que utilizam essa funcao:
+# remove_concept.php
+###################################################################
+if (!(function_exists("inserirPedidoRemoverConceito"))) {
+
+    function inserirPedidoRemoverConceito($id_projeto, $id_conceito, $id_usuario) {
+        $DB = new PGDB ();
+        $insere = new QUERY($DB);
+        $select = new QUERY($DB);
+        $select2 = new QUERY($DB);
+        $select->execute("SELECT * FROM conceito WHERE id_conceito = $id_conceito");
+        $conceito = $select->gofirst();
+        $nome = $conceito['nome'];
+
+        $insere->execute("INSERT INTO pedidocon (id_projeto,id_conceito,nome,id_usuario,tipo_pedido,aprovado) VALUES ($id_projeto,$id_conceito,'$nome',$id_usuario,'remover',0)");
+        $select->execute("SELECT * FROM usuario WHERE id_usuario = $id_usuario");
+        $select2->execute("SELECT * FROM participa WHERE gerente = 1 and id_projeto = $id_projeto");
+
+        if ($select->getntuples() == 0 && $select2->getntuples() == 0) {
+            echo "<BR> [ERRO]Pedido nao foi comunicado por e-mail.";
+        } else {
+            $record = $select->gofirst();
+            $nome = $record['nome'];
+            $email = $record['email'];
+            $record2 = $select2->gofirst();
+            while ($record2 != 'LAST_RECORD_REACHED') {
+                $id = $record2['id_usuario'];
+                $select->execute("SELECT * FROM usuario WHERE id_usuario = $id");
+                $record = $select->gofirst();
+                $mailGerente = $record['email'];
+                mail("$mailGerente", "Pedido de Remover Conceito", "O usuario do sistema $nome2\nPede para remover o conceito $id_conceito \nObrigado!", "From: $nome\r\n" . "Reply-To: $email\r\n");
+                $record2 = $select2->gonext();
+            }
+        }
+    }
+
+}
+
+###################################################################
+# Essa funcao recebe um id de conceito e remove todos os seus
+# links e relacionamentos existentes.
+#########
+##########################################################
+if (!(function_exists("removeConceito"))) {
+
+    function removeConceito($id_projeto, $id_conceito) {
+        $DB = new PGDB ();
+        $sql = new QUERY($DB);
+        $sql2 = new QUERY($DB);
+        $sql3 = new QUERY($DB);
+        $sql4 = new QUERY($DB);
+        $sql5 = new QUERY($DB);
+        $sql6 = new QUERY($DB);
+        $sql7 = new QUERY($DB);
+        # Este select procura o cenario a ser removido
+        # dentro do projeto
+
+        $sql2->execute("SELECT * FROM conceito WHERE id_projeto = $id_projeto and id_conceito = $id_conceito");
+        if ($sql2->getntuples() == 0) {
+            //echo "<BR> Cenario nao existe para esse projeto." ;
+        } else {
+            $record = $sql2->gofirst();
+            $nomeConceito = $record['nome'];
+            # tituloCenario = Nome do cenario com id = $id_cenario
+        }
+        # [ATENCAO] Essa query pode ser melhorada com um join
+        //print("<br>SELECT * FROM cenario WHERE id_projeto = $id_projeto");
+        /*  $sql->execute ("SELECT * FROM cenario WHERE id_projeto = $id_projeto AND id_cenario != $tituloCenario");
+          if ($sql->getntuples() == 0){
+          echo "<BR> Projeto n�o possui cenarios." ;
+          }else{ */
+        $qr = "SELECT * FROM conceito WHERE id_projeto = $id_projeto AND id_conceito != $id_conceito";
+        //echo($qr)."          ";
+        $qrr = mysql_query($qr) or die("Erro ao enviar a query de SELECT<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
+        while ($result = mysql_fetch_array($qrr)) {
+            # Percorre todos os cenarios tirando as tag do conceito
+            # a ser removido
+            //$record = $sql->gofirst ();
+            //while($record !='LAST_RECORD_REACHED'){
+            $idConceitoRef = $result['id_conceito'];
+            $nomeAnterior = $result['nome'];
+            $descricaoAnterior = $result['descricao'];
+            $namespaceAnterior = $result['namespace'];
+            #echo        "/<a title=\"Cen�rio\" href=\"main.php?t='c'&id=$id_cenario>($tituloCenario)<\/a>/mi"  ;
+            #$episodiosAnterior = "<a title=\"Cen�rio\" href=\"main.php?t=c&id=38\">robin</a>" ;
+            /* "'<a title=\"Cen�rio\" href=\"main.php?t=c&id=38\">robin<\/a>'si" ; */
+            $tiratag = "'<[\/\!]*?[^<>]*?>'si";
+            //$tiratagreplace = "";
+            //$tituloCenario = preg_replace($tiratag,$tiratagreplace,$tituloCenario);
+            $regexp = "/<a[^>]*?>($nomeConceito)<\/a>/mi"; //rever
+            $replace = "$1";
+            //echo($episodiosAnterior)."   ";
+            //$tituloAtual = $tituloAnterior ;
+            //*$tituloAtual = preg_replace($regexp,$replace,$tituloAnterior);*/
+            $descricaoAtual = preg_replace($regexp, $replace, $descricaoAnterior);
+            $namespaceAtual = preg_replace($regexp, $replace, $namespaceAnterior);
+            /* echo "ant:".$episodiosAtual ;
+              echo "<br>" ;
+              echo "dep:".$episodiosAnterior ; */
+            // echo($tituloCenario)."   ";
+            // echo($episodiosAtual)."  ";
+            //print ("<br>update cenario set objetivo = '$objetivoAtual',contexto = '$contextoAtual',atores = '$atoresAtual',recursos = '$recursosAtual',episodios = '$episodiosAtual' where id_cenario = $idCenarioRef ");
+            $sql7->execute("update conceito set descricao = '$descricaoAtual', namespace = '$namespaceAtual' where id_conceito = $idConceitoRef ");
+
+            //$record = $sql->gonext() ;
+            // }
+        }
+
+        # Remove o conceito escolhido
+        $sql6->execute("DELETE FROM conceito WHERE id_conceito = $id_conceito");
+        $sql6->execute("DELETE FROM relacao_conceito WHERE id_conceito = $id_conceito");
+    }
+
+}
+###################################################################
+# Processa um pedido identificado pelo seu id.
+# Recebe o id do pedido.(1.1)
+# Faz um select para pegar o pedido usando o id recebido.(1.2)
+# Pega o campo tipo_pedido.(1.3)
+# Se for para remover: Chamamos a funcao remove();(1.4)
+# Se for para alterar: Devemos (re)mover o cenario e inserir o novo.
+# Se for para inserir: chamamos a funcao insert();
+###################################################################
+if (!(function_exists("tratarPedidoConceito"))) {
+
+    function tratarPedidoConceito($id_pedido) {
+        $DB = new PGDB ();
+        $select = new QUERY($DB);
+        $delete = new QUERY($DB);
+        $select->execute("SELECT * FROM pedidocon WHERE id_pedido = $id_pedido");
+        if ($select->getntuples() == 0) {
+            echo "<BR> [ERRO]Pedido invalido.";
+        } else {
+            $record = $select->gofirst();
+            $tipoPedido = $record['tipo_pedido'];
+            if (!strcasecmp($tipoPedido, 'remover')) {
+                $id_conceito = $record['id_conceito'];
+                $id_projeto = $record['id_projeto'];
+                removeConceito($id_projeto, $id_conceito);
+            } else {
+
+                $id_projeto = $record['id_projeto'];
+                $nome = $record['nome'];
+                $descricao = $record['descricao'];
+                $namespace = $record['namespace'];
+
+                if (!strcasecmp($tipoPedido, 'alterar')) {
+                    $id_cenario = $record['id_conceito'];
+                    removeConceito($id_projeto, $id_conceito);
+                }
+                adicionar_conceito($id_projeto, $nome, $descricao, $namespace);
+            }
+        }
+    }
+
+}
+?>
