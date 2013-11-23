7d5198b50df5fa8bd44f27e9f50e01c22fd22392
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-11-23 16:34:42 -0200

    Translating scenario commentary updUser.php

--- cel/aplicacao/updUser.php
+++ cel/aplicacao/updUser.php
@@ -8,11 +8,23 @@ include_once 'bd.inc';
 $id_user = $_SESSION['id_usuario_corrente'];
 
 $database_conection = database_connect() or die("Erro ao conectar ao SGBD");
+
+/*
+ * Scenario - Update registered information
+ * Objective:   Allow the user to alter his registered data
+ * Context:     Open system, user must have logged into the system, user wishes
+ *              to update his registered data
+ *              Pre-condition: User has accessed the system
+ * Actors:      User, system
+ * Resources:   Interface
+ * Episodes:    1- The user alters the desired data
+ *              2- User clicks on the UPDATE button
+ */
 ?>
 
 <html>
     <head>
-        <title>Alterar dados de Usuário</title>
+        <title>Change user data</title>
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     </head>
 
@@ -20,25 +32,14 @@ $database_conection = database_connect() or die("Erro ao conectar ao SGBD");
     <body>
 
         <?php
-// Cenário - Alterar cadastro
-//
-//Objetivo:  Permitir ao usuário realizar alteração nos seus dados cadastrais	
-//Contexto:  Sistema aberto, Usuário ter acessado ao sistema e logado 
-//           Usuário deseja alterar seus dados cadastrais 
-//           Pré-Condição: Usuário ter acessado ao sistema	
-//Atores:    Usuário, Sistema.	
-//Recursos:  Interface	
-//Episódios: O usuário altera os dados desejados
-// 	     Usuário clica no botão de atualizar
-
         $password_cript = md5($user_password);
         $user = "UPDATE usuario SET  nome ='$name' , login = '$user_login' , email = '$user_email' , senha = '$password_cript' WHERE  id_usuario='$id_user'";
 
-        mysql_query($user) or die("<p style='color: red; font-weight: bold; text-align: center'>Erro!Login ja existente!</p><br><br><center><a href='JavaScript:window.history.go(-1)'>Voltar</a></center>");
+        mysql_query($user) or die("<p style='color: red; font-weight: bold; text-align: center'>Error!Login already exists!</p><br><br><center><a href='JavaScript:window.history.go(-1)'>Return</a></center>");
         ?>
 
-    <center><b>Cadastro atualizado com sucesso!</b></center>
-    <center><button onClick="javascript:window.close();">Fechar</button></center>
+    <center><b>Information successfully updated!</b></center>
+    <center><button onClick="javascript:window.close();">Close</button></center>
 
 
 </body>
