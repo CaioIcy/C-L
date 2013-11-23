d835ca711381fa4be716844ced36aca9794d5e7e
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-11-23 17:03:48 -0200

    Translating scenario commentary in recuperarXML.php

--- cel/aplicacao/recuperarXML.php
+++ cel/aplicacao/recuperarXML.php
@@ -4,40 +4,40 @@ session_start();
 include_once 'funcoes_genericas.php';
 include_once 'httprequest.inc';
 
-check_user_authentication("index.php");        
+check_user_authentication("index.php");
 
 $XML = "";
 ?>
 <html>
     <body>
     <head>
-        <title>Recuperar XML</title>
+        <title>Recover XML log</title>
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">        
     </head>
 
-<?php
+    <?php
+    /*
+     * Scenario - Generate XML logs
+     * Objective:   Allow the administrator to generate XML logs of a project, identified by date
+     * Context:     Manager wishes to generate a log for one of the projects, on which he is an administrator.
+     *              Pre-condition: Manager is logged in, project is registered
+     * Actors:      Administrator
+     * Resources:   System, log data, registered project data, database
+     * Episodes:    1- Recover the data from the database, and parse them to XML.
+     *              2- Display a XSL
+     */
 
-//Cen�rio -  Gerar Relat�rios XML 
-//Objetivo:     Permitir ao administrador gerar relat�rios em formato XML de um projeto,
-//              identificados por data.
-//Contexto:     Gerente deseja gerar um relat�rio para um dos projetos da qual � administrador.
-//Pr�-Condi��o: Login, projeto cadastrado.
-//Atores:       Administrador
-//Recursos:     Sistema, dados do relat�rio, dados cadastrados do projeto, banco de dados.
-//Epis�dios:    Restri��o: Recuperar os dados em XML do Banco de dados e os transformar
-//                       por uma XSL para a exibi��o
-
-$database_recuperation = database_connect() or die("Erro ao conectar ao SGBD");
-if (isset($erase)) {
-    if ($erase) {
-        $query_erase = "DELETE FROM publicacao WHERE id_projeto = '$id_project' AND versao = '$version' ";
-        $query_r_erase = mysql_query($query_erase);
+    $database_recuperation = database_connect() or die("Error while connecting to the database");
+    if (isset($erase)) {
+        if ($erase) {
+            $query_erase = "DELETE FROM publicacao WHERE id_projeto = '$id_project' AND versao = '$version' ";
+            $query_r_erase = mysql_query($query_erase);
+        }
     }
-}
-$query_database_command = "SELECT * FROM publicacao WHERE id_projeto = '$id_project'";
-$query_connecting_database = mysql_query($query_database_command) or die("Erro ao enviar a query");
-?>
-    <h2>Recupera XML/XSL</h2><br>
+    $query_database_command = "SELECT * FROM publicacao WHERE id_projeto = '$id_project'";
+    $query_connecting_database = mysql_query($query_database_command) or die("Error while sending the query");
+    ?>
+    <h2>Recover XML/XSL</h2><br>
     <?php
     while ($result = mysql_fetch_row($query_connecting_database)) {
         $data = $result[1];
@@ -46,21 +46,18 @@ $query_connecting_database = mysql_query($query_database_command) or die("Erro a
         ?>
         <table>
             <tr>
-                <th>Vers�o:</th><td><?= $version ?></td>
+                <th>Version:</th><td><?= $version ?></td>
                 <th>Data:</th><td><?= $data ?></td>
                 <th><a href="mostraXML.php?id_projeto=<?= $id_project ?>&versao=<?= $version ?>">XML</a></th>
-                <th><a href="recuperarXML.php?id_projeto=<?= $id_project ?>&versao=<?= $version ?>&apaga=true">Apaga XML</a></th>
-
+                <th><a href="recuperarXML.php?id_projeto=<?= $id_project ?>&versao=<?= $version ?>&apaga=true">Erases XML</a></th>
             </tr>
-
-
         </table>
 
-    <?php
-}
-?>
+        <?php
+    }
+    ?>
 
-    <br><i><a href="showSource.php?file=recuperarXML.php">Veja o c�digo fonte!</a></i>
+    <br><i><a href="showSource.php?file=recuperarXML.php">See the source code!</a></i>
 
 </body>
 
