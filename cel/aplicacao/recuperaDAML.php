fecb6af1cc40e6444bc996bf1ea6c5c773b9a74b
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-11-23 17:07:45 -0200

    Refactoring recuperaDAML.php

--- cel/aplicacao/recuperaDAML.php
+++ cel/aplicacao/recuperaDAML.php
@@ -1,19 +1,18 @@
+<?php
+// File: recuperaDAML.php
+// Purpose: This module lists all the generated DAML files on $_SESSION['diretorio']
+?>
+
 <HTML> 
     <HEAD> 
         <LINK rel="stylesheet" type="text/css" href="style.css"> 
-        <TITLE>Recupera��o de Arquivos DAML</TITLE> 
+        <TITLE>DAML files recovery</TITLE> 
     </HEAD> 
 
     <BODY> 
-        <H2>Hist�rico de Arquivos DAML</H2> 
+        <H2>DAML files history</H2> 
         <?php
-        
         include_once 'CELConfig/CELConfig.inc';
-        
-         // Arquivo   : recuperaDAML.php
-         // Vers�o       : 1.0
-         // Coment�rio: Este programa lista todos os arquivos DAML    gerados    em $_SESSION['diretorio']
-         
 
         function extract_data($file_name) {
             list($project, $rest) = split("__", $file_name);
@@ -53,7 +52,7 @@
             return $day . " de " . $months_written . " de " . $year . " �s " . $hour . ":" . $minute . "." . $second . "\n";
         }
 
-        function extract_projeto($file_name) {
+        function extract_project($file_name) {
             list($project) = split("__", $file_name);
             return $project;
         }
@@ -62,26 +61,24 @@
         $site = $_SESSION['site'];
 
         if ($directory == "") {
-            //    $diretorio =  "teste"; 
             $directory = CELConfig_ReadVar("DAML_dir_relativo_ao_CEL");
         }
 
         if ($site == "") {
-            //    $site = "http://139.82.24.189/cel_vf/aplicacao/teste/";
             $site = "http://" . CELConfig_ReadVar("HTTPD_ip") . "/" . CELConfig_ReadVar("CEL_dir_relativo") . CELConfig_ReadVar("DAML_dir_relativo_ao_CEL");
             if ($site == "http:///") {
                 print( "Aten��o: O arquivo de configura��o do CELConfig (padr�o: config2.conf) precisa ser configurado corratamente.<BR>\n * N�o foram preenchidas as vari�veis 'HTTPD_ip','CEL_dir_relativo' e 'DAML_dir_relativo_ao_CEL'.<BR>\nPor favor, verifique o arquivo e tente novamente.<BR>\n");
             }
         }
 
-        // Monta a tabela    de arquivos    DAML 
+        // Assembles the table with DAML files
         print( "<CENTER><TABLE WIDTH=\"80%\">\n");
         print( "<TR>\n\t<Th><STRONG>Projeto</STRONG></Th>\n\t<Th><STRONG>Gerado em</STRONG></Th>\n</TR>\n");
         if ($directory_handle = @opendir($directory)) {
             while (( $file = readdir($directory_handle) ) !== false) {
                 if (is_file($directory . "/" . $file) && $file != "." && $file != "..") {
                     print( "<TR>\n");
-                    print( "\t<TD WIDTH=\"25%\" CLASS=\"Estilo\"><B>" . extract_projeto($file) . "</B></TD>\n");
+                    print( "\t<TD WIDTH=\"25%\" CLASS=\"Estilo\"><B>" . extract_project($file) . "</B></TD>\n");
                     print( "\t<TD WIDTH=\"55%\" CLASS=\"Estilo\">" . extract_data($file) . "</TD>\n");
                     print( "\t<TD WIDTH=\"10%\" >[<A HREF=\"" . $site . $file . "\">Abrir</A>]</TD>\n");
                     print( "</TR>\n");
