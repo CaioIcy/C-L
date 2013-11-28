e4fbca8a2033c9f09107b5654e9a3880fe3d7186
CaioIcy <caio.nardelli@hotmail.com>
2013-11-27 22:18:17 -0200

    Translating mostraXML.php

--- cel/aplicacao/mostraXML.php
+++ cel/aplicacao/mostraXML.php
@@ -5,23 +5,25 @@ session_start();
 include_once 'funcoes_genericas.php';
 include_once 'httprequest.inc';
 
-chechUserAuthentication("index.php");        // Checa se o usuario foi autenticado
+chechUserAuthentication("index.php");
 
-$database_recuperation = database_connect() or die("Erro ao conectar ao SGBD");
+$database_recuperation = database_connect() or die("Error while connecting to the database.");
 
-//Cen�rio -  Gerar Relat�rios XML 
-//Objetivo:	Permitir ao administrador gerar relat�rios em formato XML de um projeto,
-//              identificados por data.
-//Contexto:     Gerente deseja gerar um relat�rio para um dos projetos da qual � administrador.
-//Pr�-Condi��o: Login, projeto cadastrado.
-//Atores:	Administrador
-//Recursos:	Sistema, dados do relat�rio, dados cadastrados do projeto, banco de dados.
-//Epis�dios:    Gerando com sucesso o relat�rio a partir dos dados cadastrados do projeto,
-//              o sistema fornece ao administrador a tela de visualiza��o do relat�rio
-//              XML criado
+/*
+ * Scenario - Generate XML logs
+ * Objective:   Allow the administrator to generate logs in XML format from a project,
+ *              that is identified by date.
+ * Context:     Manager wishes to generate a log from one of the project he is an
+ *              administrator of.
+ *              Pre-condition: Logged in, project is registered
+ * Actors:      Administrator
+ * Resources:   System, log data, data from registered project, database
+ * Episodes:    1- From the data of the regisstered project, the system supplies to
+ *                  the administrator a screen to visualize the created XML
+ */
 
 $query = "select * from publicacao where id_projeto = $id_project AND versao = $version";
-$query_connecting_database = mysql_query($query) or die("Erro ao enviar a query");
+$query_connecting_database = mysql_query($query) or die("Error while sending query.");
 $row = mysql_fetch_row($query_connecting_database);
 $xml_base = $row[3];
 
