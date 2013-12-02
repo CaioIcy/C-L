66991d876c8470e5e6a98c6968013c6d1c8b2f11
paulohtfs <paulohtfs@gmail.com>
2013-12-02 11:36:45 -0200

    Evolving test ProjectDAOTest.php

--- Teste/cel/aplicacao/DAO/ProjectDAOTest.php
+++ Teste/cel/aplicacao/DAO/ProjectDAOTest.php
@@ -7,6 +7,10 @@ class ProjectDAOTest extends PHPUnit_Framework_TestCase {
 
     protected function setUp() {
         database_connect();
+        $this->setUpProject();
+    }
+    
+    protected function setUpProject(){
         $queryProject = "INSERT INTO  projeto (id_projeto,  nome, 
             data_criacao,  descricao,  id_status) VALUES ( 9999,
             'ProjectName', 2013 -11 -25,  'DescProject', 9999 )";
@@ -14,15 +18,24 @@ class ProjectDAOTest extends PHPUnit_Framework_TestCase {
     }
 
     protected function tearDown() {
+        $this->tearDownProject();
+    }
+    
+    protected function tearDownProject(){
         $queryDelProject = "DELETE FROM projeto WHERE id_projeto='9999'";
         mysql_query($queryDelProject);
     }
-    
+
+
     public function testGetProjectNameDatabase(){
         $resultProject = getProjectNameDatabase("ProjectName");
         $this->assertEquals("ProjectName",$resultProject[1]);
     }
-
+    
+    public function testGetProjectIdDatabase(){
+        $resultId = getProjectIdDatabase(9999, 9999);
+        $this->assertEquals(9999,$resultId);
+    }
 }
 
 ?>
